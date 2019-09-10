2ad78cbe3a7a812b6c0062d52c451037a4c781cf
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-10 02:58:59 -0400

    Changed to blue background for the records list card

--- src/components/PeopleRecords.js
+++ src/components/PeopleRecords.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 class PeopleRecords extends Component {
     render() {
         return (
-            <div>
-               <div className="card" style="width: 18rem;">
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Cras justo odio</li>
-                    <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li>
-                </ul>
+            <div className="container mt-5">
+                <h3>List of Personal Records</h3>
+               <div class="card" style={{width: "18rem"}}>
+                <div class="card-header bg-primary text-white text-uppercase">
+                    Featured
+                </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Cras justo odio</li>
+                        <li className="list-group-item">Dapibus ac facilisis in</li>
+                        <li className="list-group-item">Vestibulum at eros</li>
+                    </ul>
                 </div>
             </div>
         )
