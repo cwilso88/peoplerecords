4d6ec29c40af47fb93e2ca187817128e312799ff
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-09 21:36:13 -0400

    Created the PeopleRecords components

---
+++ src/components/PeopleRecords.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PeopleRecords extends Component {
+    render() {
+        return (
+            <div>
+               PeopleRecords 
+            </div>
+        )
+    }
+}
+
+export default PeopleRecords;
