7685334b13533253ee804c0fb0cce165c79693df
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-10 11:13:13 -0400

    Hard coded data into the CharacterCount until the CORS preflight is passes

--- src/components/CharacterCount.js
+++ src/components/CharacterCount.js
@@ -7,29 +7,22 @@ class CharacterCount extends Component {
                 <table class="table table-bordered">
                     <thead>
                         <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                            <th scope="col">Character</th>
+                            <th scope="col">Count</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <th scope="row">R</th>
+                        <td>20</td>
                         </tr>
                         <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
+                        <th scope="row">L</th>
+                        <td>1</td>
                         </tr>
                         <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <th scope="row">M</th>
+                        <td>10</td>
                         </tr>
                     </tbody>
                 </table>
