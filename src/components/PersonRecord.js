537f7c88a7e08b2a596311959b860b1015becaac
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-09 21:38:43 -0400

    Created the PersonRecord to contain the individual records for the people's list

---
+++ src/components/PersonRecord.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PersonRecord extends Component {
+    render() {
+        return (
+            <div>
+                PersonRecord
+            </div>
+        )
+    }
+}
+
+export default PersonRecord;
