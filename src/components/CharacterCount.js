bc0615c35418524d859652bdb42673404c8886e0
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-09 21:40:49 -0400

    Created the CharacterCount component to contain the table for character counts and the button

---
+++ src/components/CharacterCount.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CharacterCount extends Component {
+    render() {
+        return (
+            <div>
+                CharacterCount
+            </div>
+        )
+    }
+}
+
+export default CharacterCount;
