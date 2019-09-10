ee073b746a034992a1df80c7c077891d76c7e5a7
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-09 22:03:35 -0400

    Imported each component into the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import CharacterCount from './components/CharacterCount';
+import Menu from './components/Menu';
+import PeopleRecords from './components/PeopleRecords';
+import PersonRecord from './components/PersonRecord';
 
 function App() {
   return (
     <div className="App">
      SalesLoft People Records
+     <Header />
     </div>
   );
 }
