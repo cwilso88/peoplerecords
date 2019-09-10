4486da46bc71f7608e504997cd0a506a8e06217c
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-10 06:35:12 -0400

    installed dotenv

--- src/components/PersonRecord.js
+++ src/components/PersonRecord.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import getRecordList from '../api/api.js';
+
 class PersonRecord extends Component {
     state = {
         people: []
     }
 
     componentDidMount() {
-        axios.get()
+        getRecordsList.get('')
     }
 
     render() {
