2dadf9e205a19d78baee855c9916f7d4f0ab3830
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-09 22:38:43 -0400

    Added Bootstrap navbar for the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Menu = () => {
     return (
         <div>
-            Menu
+            <nav className="navbar navbar-light bg-light">
+                <form className="form-inline">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon1">@</span>
+                        </div>
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    </div>
+                </form>
+            </nav>
         </div>
     )
 }
