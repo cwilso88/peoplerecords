89a968c6d7698f38fb32109d0b32f492910835b6
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-17 09:23:42 -0400

    Updated the component name PersonRecordsList to better describe the comp functionality or purpose

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import CharacterCount from './components/CharacterCount';
 import Menu from './components/Menu';
-import PeopleRecords from './components/PeopleRecords';
+import PeopleRecords from './components/PeopleRecordsList';
 //import PersonRecord from './components/PersonRecord';
 import axios from 'axios';
 import './css/main.css';
