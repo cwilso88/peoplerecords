e46d7e4bc6a23bd2c5d711295e72ffa33a787b32
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-09-17 09:39:53 -0400

    Refactoring the PeopleRecordItem component

--- src/components/PersonRecordItem.js
+++ src/components/PersonRecordItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-
-
-class PeopleRecords extends Component {   
-    render() {
+const PeopleRecordItem = ({ person }) => {   
+    if(!person) {
+        return(
+            <div>
+                Looking for your records...
+            </div>
+        )
+    } else {
         const { id, name, email, phone, department, status, location, image } = this.props.getPeopleRecords;
-        
         return (
             <div id="PeopleRecords" className="container mt-5">
                 <h3>List of Personal Records</h3>
@@ -22,33 +25,9 @@ class PeopleRecords extends Component {
                         </ul>
                     </div>
                </div>
-               <div className="border border-secondary bg-light justify-items-center">
-                <div className="card m-5" style={{width: "28rem"}}>
-                    <div className="card-header text-white text-uppercase">
-                        Full Name: Sally Joe
-                    </div>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Job Title: Engineer</li>
-                            <li className="list-group-item"><i className="fas fa-envelope-square pr-2"></i>Email: sally@gmail.com</li>
-                            <li className="list-group-item"><i className="far fa-address-card pr-2"></i>Address: Atlanta, GA</li>
-                        </ul>
-                    </div>
-               </div>
-               <div className="border border-secondary bg-light justify-items-center">
-                <div className="card m-5" style={{width: "28rem"}}>
-                        <div className="card-header text-white text-uppercase">
-                            Full Name: Tom Wilson
-                        </div>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">Job Title: Engineer</li>
-                                <li className="list-group-item"><i className="fas fa-envelope-square pr-2"></i>Email: tom@gmail.com</li>
-                                <li className="list-group-item"><i className="far fa-address-card pr-2"></i>Address: Atlanta, GA</li>
-                            </ul>
-                        </div>
-                </div>
             </div>
         )
-    }
+    }   
 }
 
-export default PeopleRecords;
+export default PeopleRecordItem;
